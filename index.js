7985d88b5cf1b79140865073a6604ca0f8c4c072
odaney baker <bakerobtb@gmail.com>
2023-03-07 19:47:53 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(array,eventName)
+{    
+    const newarray = [];  
+    for (let i = 0; i < array.length; i++)
+    {    
+       newarray[i] = (`Thank you, ${array[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return newarray;
+}
+
+function countDown(num){
+    let count = num;
+    while (count >= 0){
+        console.log(count)
+        count--;
+    }
+}
